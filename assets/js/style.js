efb1ab3e64cc3b2bc178a421513de79010713826
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-08-05 22:09:34 -0700

    fixing some js and css

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,7 @@
 const startButton = document.getElementById("start-btn")
 const questionContainerElement = document.getElementById("question-container")
+// fix this code
+const shuffledQuestions, currentQuestion
 
 startButton.addEventListener("click", startGame)
 
@@ -7,9 +9,20 @@ function startGame() {
     console.log("started")
     startButton.classList.add("hide")
     questionContainerElement.classList.remove("hide")
+    setNextQUestion()
 }
 
 function selectAnswer() {
 
 
 }
+
+const questions = [
+    {
+        question: "which ohio state legend is the only 2 time Heisman Trophy Winner?",
+        answers: [
+            {text: "troy smith", correct: false}, 
+            {text: "archie griffin", correct: true}
+        ]
+    }
+]
