57d33e9848a8ed5479fdb5aa7465480528edc977
Lawrencecrr <lawrenceschoolmail@gmail.com>
2022-08-09 15:09:36 -0400

    finished js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,11 +1,13 @@
 function populateHighScores() {
     // get array from storage, or initialize as empty array
-    let highScoresList = JSON.parse(localStorage.getItem('highScores')) || [];
+    let highScoresList = JSON.parse(localStorage.getItem('scores')) || [];
     // populate highscores list
     let list = '';
+
     highScoresList.forEach(score => {
         list = list + '<p>' + score.initials + '  :  ' + score.score + '</p>';
     });
+
     highScoresListEl.innerHTML = list;
 }
 
